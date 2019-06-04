da8c47f9c516798386c735cfcd8aa389298b637c
Manoel <manoel.lobo@gmail.com>
2019-06-04 09:21:43 -0300

    Apollo with react-apollo and Query component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,44 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Query } from "react-apollo"
+import gql from "graphql-tag"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const CHARACTER_QUERY = gql`
+  {
+    character(id: 1) {
+      name
+      species
+      gender
+      origin {
+        name
+      }
+    }
+  }
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <p>The content below is using react-apollo with the Query component</p>
+    <Query query={CHARACTER_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>
+        if (error) return <p>Error: {error.message}</p>
+
+        const { name, species, gender, origin } = data.character
+
+        return (
+          <p>
+            This is the info about {name}, a {species} {gender} from{" "}
+            {origin.name}{" "}
+          </p>
+        )
+      }}
+    </Query>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
